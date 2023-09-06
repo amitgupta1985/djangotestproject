d497fb85da60f2105bef13705833dc15be37a5c8
amitgupta1985 <44477035+amitgupta1985@users.noreply.github.com>
2023-09-06 16:47:32 +0530

    Update settings.py

--- restapi/settings.py
+++ restapi/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret hello!
 SECRET_KEY = 'django-insecure-8vzhfmvltmpgw_wasb(%j+5#_qzt4brc0c6ih5^!bju=wo#gz5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
